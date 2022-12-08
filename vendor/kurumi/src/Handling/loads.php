e24f1af19c9b11daa02644ec8e2c0410cd5959db
bydefaultiloveyou <mikomeysa05@gmail.com>
2022-12-08 21:36:20 +0700

    udah nih aulia sayang

--- vendor/kurumi/src/Handling/loads.php
+++ vendor/kurumi/src/Handling/loads.php
@@ -5,6 +5,6 @@ class loads
   public static function showError(string $filename)
   {
     $message = "trying to access a file that doesn't exist.";
-    require_once "./vendor/kurumi/handling/message/index.php";
+    require_once "./vendor/kurumi/src/Handling/Web/index.php";
   }
 }
