b0e877be91f427c582bd0cf3fb68da1d4cbafe49
bydefaultiloveyou <mikomeysa05@gmail.com>
2022-12-08 21:26:06 +0700

    udah nih aulia sayang

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -2,8 +2,8 @@
 spl_autoload_register(function ($class) {
   $class = explode('\\', $class);
   $class = end($class);
-  if (file_exists('./vendor/Kurumi/Src/Http/' . $class . '.php')) {
-    require_once './vendor/Kurumi/Src/Http/' . $class . '.php';
+  if (file_exists('./vendor/Kurumi/src/Http/' . $class . '.php')) {
+    require_once './vendor/Kurumi/src/Http/' . $class . '.php';
   } else {
     require_once './app/Controllers/' . $class . '.php';
   }
@@ -14,7 +14,7 @@ spl_autoload_register(function ($class) {
   | si function spl_autoload_register ngak jalan
  */
 
-require_once "./vendor/kurumi/Func/resource.php";
+require_once "./vendor/kurumi/src/Func/resource.php";
 require_once './vendor/kurumi/Src/Handling/loads.php';
-require_once './vendor/kurumi/Func/view.php';
+require_once './vendor/kurumi/src/Func/view.php';
 require_once './routes/web.php';
