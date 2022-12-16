071f7354e3f528ea6996e7a5a2a8bbf3294f9174
lutfi <biolutfi@gmail.com>
2022-12-16 16:50:12 +0700

    delete yang ngak penting

--- vendor/kurumi/src/Templates/Template.php
+++ vendor/kurumi/src/Templates/Template.php
@@ -5,21 +5,18 @@ namespace Kurumi\Templates;
 
 class Template
 {
-  protected $path;
-  protected $data;
-  protected $file_name;
+  protected $path,
+    $file_name;
 
-  public function __construct($path, $file_name, $data)
+  public function __construct($path, $file_name)
   {
     $this->path = $path . '.kurumi.php';
     $this->file_name = $file_name;
-    $this->data = $data;
     $this->syntax();
   }
 
   public function syntax()
   {
-    $data = $this->data;
     $contents = file_get_contents($this->path);
     $contents = preg_replace('/\{{ (.*) \}}/', '<?= $1 ?>', $contents);
     $file_new = fopen(__DIR__ . '/../../../../storage/framework/views/' . $this->file_name . '.kurumi.php', 'w');
