fc214b539f5a0bfb6b0192091efa317784d08817
lutfi <biolutfi@gmail.com>
2022-12-17 17:43:14 +0700

    asset

--- storage/framework/views/components.layouts.kurumi.php
+++ storage/framework/views/components.layouts.kurumi.php
@@ -6,11 +6,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Bootstrap demo</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <link href="<?php asset('css/styles.css') ?>" rel="stylesheet" />
+  <link href=<?php echo "./public/" . "style.css"  ?> rel="stylesheet" />
 </head>
 
 <body>
-  <?php include __DIR__ . "/" . 'components.navbar' . ".kurumi.php" ?>
+  <?php include __DIR__ . "/" . 'components.navbar' . ".kurumi.php" ?>
   <?php include $slot ?>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
