441b0fbc41adcfb6d0014d699dda1afae3b4d088
iqbalthex <73935864+iqbalthex@users.noreply.github.com>
2022-12-23 18:23:16 +0700

    refaktor kode jadi lebih efisien dan mudah dibaca
    bagi saya :v

--- vendor/kurumi/src/Database/DB.php
+++ vendor/kurumi/src/Database/DB.php
@@ -13,63 +13,56 @@ use Kurumi\Database\Validate;
 
 class DB extends Validate
 {
-
-  protected $connect;
+  protected $connection;
 
   public function __construct()
   {
     $databaseConfig = require __DIR__ . "/../../../../config/database.php";
 
-    $connect = new PDO($databaseConfig["dialect"] . ":host=" . $databaseConfig["host"] . ";dbname=" . $databaseConfig["database"], $databaseConfig["user"], $databaseConfig["password"]);
+    $connection = new PDO("$databaseConfig[dialect]:
+      host=$databaseConfig[host];dbname=$databaseConfig[database]",
+      $databaseConfig['user'], $databaseConfig['password']
+    );
 
-    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    return $this->connect = $connect;
+    return $this->connection = $connection;
   }
 
   public function create(array $query)
   {
     $this->table($query);
-
     $this->column($query);
-
     $this->value($query);
 
-    return $this->query("INSERT");
+    return $this->query('insert');
   }
 
   public function select(array $query)
   {
     $this->distinct($query);
-
     $this->join($query);
-
     $this->table($query);
-
     $this->where($query);
-
     $this->by($query);
 
-    return $this->query("SELECT");
+    return $this->query('select');
   }
 
   public function delete(array $query)
   {
     $this->table($query);
-
     $this->where($query);
 
-    return $this->query("DELETE");
+    return $this->query('delete');
   }
 
   public function update(array $query)
   {
     $this->column($query);
-
     $this->where($query);
-
     $this->table($query);
 
-    return $this->query("UPDATE");
+    return $this->query('update');
   }
 }
