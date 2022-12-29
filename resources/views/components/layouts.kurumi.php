b89285de480a377faafe66c4490ac4fc3d2f02eb
lutfi <biolutfi@gmail.com>
2022-12-29 18:52:31 +0700

    add manifest json

--- resources/views/components/layouts.kurumi.php
+++ resources/views/components/layouts.kurumi.php
@@ -4,6 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="theme-color" content="#F380AB" />
+  <link rel="manifest" href="@asset('manifest.json')" />
   <title>Kurumi Framework</title>
   @css ("https://fonts.googleapis.com/css2?family=Radio+Canada&display=swap" rel="stylesheet")
   @css ("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css")
--- storage/framework/views/components.layouts.kurumi.php
+++ storage/framework/views/components.layouts.kurumi.php
@@ -4,15 +4,14 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="theme-color" content="#F380AB" />
+  <link rel="manifest" href="<?php echo 'manifest.json' ?>" />
   <title>Kurumi Framework</title>
-  <link href="https://fonts.googleapis.com/css2?family=Radio+Canada&display=swap" rel="stylesheet" rel="stylesheet" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" />
-  <link href='css/styles.css' rel="stylesheet" />
-</head>
+  <link href="https://fonts.googleapis.com/css2?family=Radio+Canada&display=swap" rel="stylesheet" rel="stylesheet" /><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" /><link href="css/styles.css" rel="stylesheet" /></head>
 
 <body>
   <?php include $slot ?>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script></body>
 
 </html>
