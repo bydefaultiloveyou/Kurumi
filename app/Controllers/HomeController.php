59de93aa321de75e1cdca0e25e779d2d06720d33
iqbalthex <73935864+iqbalthex@users.noreply.github.com>
2022-12-30 10:07:29 +0700

    modify documentation

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,13 +1,12 @@
 <?php
 
-/* 
-|--------------------------------------------------------------------------
-| Controllers 
-|--------------------------------------------------------------------------
- */
-
 namespace App\Controllers;
 
+
+/**
+ * Disini kamu bisa mengatur operasi yang dilakukan
+ * sesuai dengan route yang sudah dibuat.
+ */
 class HomeController
 {
     public static function home()
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -1,9 +1,13 @@
 <?php
 
 namespace App\Models;
-
 use Kurumi\Database\Database;
 
+
+/**
+ * Disini kamu bisa menuliskan logika dan operasi
+ * yang biasa dilakukan pada database (misalnya query).
+ */
 class Task
 {
   public static function DB()
