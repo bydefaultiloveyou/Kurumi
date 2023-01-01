8a7cd924e9725dfcc7dfcadde3678750056db06e
iqbalthex <73935864+iqbalthex@users.noreply.github.com>
2023-01-02 05:25:25 +0700

    Update File.php

--- vendor/kurumi/Kurumi/File.php
+++ vendor/kurumi/Kurumi/File.php
@@ -1,86 +1,84 @@
 <?php
 
 namespace Kurumi\Kurumi;
-use Kurumi\Kurumi\TemplateEngine;
+use Kurumi\Kurumi\Haniel;
 
 /**
  * Class untuk mengelola file dan directive
  */
-class File
-{
+class File {
   /**
-   * @var array $files # berisi nama-nama file dari folder input
+   * @var array $input_files  # berisi nama-nama file dari folder input
+   * @var array $output_files # berisi nama-nama file dari folder output
    */
-  private array $files = [];
+  private array $input_files  = [];
+  private array $output_files = [];
 
   /**
    * @var $input_directory  # nama folder input
    * @var $output_directory # nama folder output
    */
-  private $input_directory = __DIR__ . '/../../../resources/views/';
+  private $input_directory  = __DIR__ . '/../../../resources/views/';
   private $output_directory = __DIR__ . '/../../../storage/framework/views/';
 
 
-  public function __construct()
-  {
-    $this->files = array_merge(
+  /**
+   * Mengambil semua file di folder input dan output.
+   * Memperbarui file view (sterilize & generate).
+   */
+  public function __construct() {
+    $this->input_files = array_merge(
       glob("{$this->input_directory}*.kurumi.php"),
       glob("{$this->input_directory}**/*.kurumi.php"),
       glob("{$this->input_directory}**/**/*.kurumi.php"),
       glob("{$this->input_directory}**/**/**/*.kurumi.php"),
     );
-    // dd($this->files);
+    $this->output_files = glob("{$this->output_directory}*.php");
+
     $this->sterilize();
     $this->generate();
   }
 
 
   /**
-   * menyesuaikan file input dan output
-   * (hapus file output bila tidak ada di folder input)
+   * Menyesuaikan file input dan output
+   * -> ambil nama file yang berakhiran '.kurumi.php'     # (str_replace)
+   * -> cek adakah file input                             # (file_exists)
+   * -> hapus file output bila tidak ada di folder input  # (unlink)
    * @return void
    */
-  public function sterilize(): void
-  {
-    // ambil nama file di folder storage/framework/views
-    $files = glob($this->output_directory . "*.php");
-
-    foreach ($files as $file)
-    {
-      // ambil nama file yang berakhiran '.kurumi.php'
-      $base_name = str_replace('.kurumi.php', '', $file);
-
-      // ubah simbol . menjadi /
-      $base_name = str_replace('.', '/', $base_name);
-      $base_name = basename($file, '.kurumi.php');
-
-      // hapus file output bila tidak ada di folder input
-      // var_dump("{$this->input_directory}{$base_name}.kurumi.php");
+  public function sterilize(): void {
+    foreach ($this->output_files as $file) {
+      $base_name = str_replace('.kurumi.php', '', basename($file));
       
-      if (!file_exists("{$this->input_directory}{$base_name}.kurumi.php"))
-      {
-        unlink("{$this->output_directory}{$base_name}.kurumi.php");
+      $output_dir = $this->output_directory . $base_name;
+      $input_dir  = $this->input_directory . str_replace('.', '/', $base_name);
+
+      if (!file_exists("$input_dir.kurumi.php")) {
+        unlink("$output_dir.kurumi.php");
       }
     }
   }
 
-  public function generate()
-  {
-    foreach ($this->files as $file)
-    {
 
-      // ambil nama file tanpa direktorinya
+  /**
+   * Method untuk membuat file baru dengan
+   * directive yang telah diubah menjadi kode php oleh Haniel.
+   * -> ambil nama file tanpa direktorinya
+   * -> ubah simbol / menjadi .     # (str_replace)
+   * -> ambil isi file              # (file_get_contents)
+   * -> buat file di folder output  # (fopen, fwrite, fclose)
+   * @return void
+   */
+  public function generate(): void {
+    foreach ($this->input_files as $file) {
       $filename = str_replace($this->input_directory, '', $file);
+      $filename = str_replace('/', '.', $filename);
 
-      // ubah simbol / menjadi .
-      $filename = str_replace("/", ".", $filename);
-
-      // ambil isi file
       $contents = file_get_contents($file);
 
-      // buat file di folder output
       $file_new = fopen($this->output_directory . $filename, 'w');
-      fwrite($file_new, TemplateEngine::parse($contents));
+      fwrite($file_new, Haniel::transform($contents));
       fclose($file_new);
     }
   }
